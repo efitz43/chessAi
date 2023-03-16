7eaff026c77ca28be5c0768262aa8f0d284e94fe
efitz43 <esf26@pitt.edu>
2023-03-15 20:02:46 -0400

    Adding base files and ideas

---
+++ board.py
@@ -0,0 +1,5 @@
+from ast import main
+
+
+if __name__ == 'main':
+    print('hello')
---
+++ piece.py
@@ -0,0 +1,50 @@
+# Need to make all the pieces
+# there possible moves
+# think about if the piece color matters
+
+class ChessPiece:
+    # Pieces can not overlap 
+        # can capture other pieces
+    # Most pieces can capture with their move
+    # Most pieces can not move through another piece
+
+    def pawn(self):
+        return False
+        # keep tract of first move
+        firstMove = True
+
+        # move one otherwise
+
+        # can only take diagonal
+    def knight(self):
+        # can go around pieces
+        # is implemented
+        return False
+
+        # can move 2 in one direction and one in the other
+        # can take when move
+    
+    def bishop(self):
+        return False
+
+        # can move any direction diagonally
+        # can take when move
+
+    def rook(self):
+        return False
+
+        # can move max lateral
+        # can move max vertical
+
+    def queen(self):
+        return False
+        # can move like a rook and knight
+
+    def king(self):
+        return False
+        # can move like a rook and knight but only one space
+
+
+
+        
+
